9bc9d9abe0ad6498296895cfb206e796119a66e3
Abrahm <abrahmrollins@gmail.com>
2024-01-25 13:01:39 -0800

    Changing ideas - Removed unneccessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import ImageHalf from './components/ImageHalf';
-import DetailHalf from './components/DetailsHalf';
 
 function App() {
   return (
     <div className="App" id="appHome">
-      <DetailHalf />
-      <ImageHalf />
+
     </div>
   );
 }
