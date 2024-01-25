d291d836ee457878833add41079ac97debd65e7d
Abrahm <abrahmrollins@gmail.com>
2024-01-25 13:54:23 -0800

    Added in Ubuntu Font. Built Welcome Text Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import WelcomeText from './components/WelcomeText';
 
 function App() {
   return (
-    <div className="App">
-
+      <div className="App">
+        <div id="splashArea">
+          <div id="personalInfo">
+            <WelcomeText />
+          </div>
+        </div>
     </div>
   );
 }
---
+++ src/components/WelcomeText.js
@@ -0,0 +1,12 @@
+
+
+function WelcomeText(){
+    return(
+        <div id="welcomeText">
+            <h1>Hi, I'm Abrahm</h1>
+            <h2>Front End Devloper & Mountaineer</h2>
+        </div>
+    )
+}
+
+export default WelcomeText;
