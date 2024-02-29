e6046ca31ee4f111a6c38277c30745885aeee558
Abrahm <abrahmrollins@gmail.com>
2024-02-29 00:22:59 -0800

    Temporarliy removed projects area for early launch - restyled the icons container and fixed responsive issues

--- src/components/ProfileIcons.js
+++ src/components/ProfileIcons.js
@@ -7,10 +7,12 @@ import ResumeIcon from '../media/resume.png';
 function ContactIcons(){
     return(
         <div className="contact-icons">
-            <a href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
-            <a href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Github Icon" /></a>
-            <a href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Github Icon" /></a>
-            <a href="tel:12063956074" target="_blank" rel="noreferrer"><img src={CellPhoneIcon} alt="Github Icon" /></a>
+            <div class="icons-box">
+                <a class="resume-icon" href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Resume Icon" /></a>
+                <a class="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
+                <a class="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={CellPhoneIcon} alt="Cellphone Icon" /></a>
+                <a class="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
+            </div>
         </div>
     )
 }
