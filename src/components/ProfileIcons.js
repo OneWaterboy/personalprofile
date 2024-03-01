f6c551c984d6c8969d2d9afd63e67aa45cb77b7c
Abrahm <abrahmrollins@gmail.com>
2024-02-29 20:09:23 -0800

    changed top bar icons and rearranged order

--- src/components/ProfileIcons.js
+++ src/components/ProfileIcons.js
@@ -1,16 +1,18 @@
 import GithubIcon from '../media/github.png';
 import EmailIcon from '../media/gmail.png';
-import CellPhoneIcon from '../media/mobile-call.png';
+import PhoneIcon from '../media/phonecall.png';
 import ResumeIcon from '../media/resume.png';
+import LinkedinIcon from '../media/linkedin.png';
 
 
 function ContactIcons(){
     return(
         <div className="contact-icons">
             <div className="icons-box">
-                <a className="resume-icon" href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Resume Icon" /></a>
                 <a className="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
-                <a className="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={CellPhoneIcon} alt="Cellphone Icon" /></a>
+                <a className="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={PhoneIcon} alt="Cellphone Icon" /></a>
+                <a className="resume-icon" href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Resume Icon" /></a>
+                <a className="linkedin-icon" href="www.linkedin.com/in/abrahmrollins" target="_blank" rel="noreferrer"><img src={LinkedinIcon} alt="Github Icon" /></a>
                 <a className="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
             </div>
         </div>
