e715c4c85184635aaccf9277dfd8432551d39837
Abrahm <abrahmrollins@gmail.com>
2024-02-29 18:11:12 -0800

    fixed class designation to reflect React syntax with className

--- src/components/ProfileIcons.js
+++ src/components/ProfileIcons.js
@@ -7,11 +7,11 @@ import ResumeIcon from '../media/resume.png';
 function ContactIcons(){
     return(
         <div className="contact-icons">
-            <div class="icons-box">
-                <a class="resume-icon" href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Resume Icon" /></a>
-                <a class="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
-                <a class="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={CellPhoneIcon} alt="Cellphone Icon" /></a>
-                <a class="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
+            <div className="icons-box">
+                <a className="resume-icon" href="Resume" target="_blank" rel="noreferrer"><img src={ResumeIcon} alt="Resume Icon" /></a>
+                <a className="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
+                <a className="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={CellPhoneIcon} alt="Cellphone Icon" /></a>
+                <a className="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
             </div>
         </div>
     )
