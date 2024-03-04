ffc62c3bf0ec4cc59c92ab738dcdb66b73ce9892
Abrahm <abrahmrollins@gmail.com>
2024-03-03 17:58:52 -0800

    Moved project object to a seperate compnent to keep code more readable and aid in troubleshooting if neccessary in the future

---
+++ src/components/Project.js
@@ -0,0 +1,29 @@
+import { projects } from '../projects';
+
+function Project(){ 
+    return(
+        <>
+            {Object.values(projects).map((project, index) => (
+            <div key={index} className="project">
+                <div className='project-screenshot'>
+                    <img src={project.img} alt={project.name} />
+                </div>
+                <div className='project-details'>
+                    <h3>{project.name}</h3>
+                    <div className='tech-badges'>
+                        {
+                            Array.isArray(project.tech) && project.tech.map((tech, index) => (
+                                <img className='tech-badge' key={index} src={tech} alt={`Tech ${index + 1}`} />
+                            ))
+                        }
+                    </div>
+                    <p>{project.desc}</p>
+                    <a target="_blank" href={project.link}>Visit {project.name}</a>
+                </div>
+            </div>
+            ))}
+        </>
+    )
+}
+
+export default Project;
--- src/components/WorkExamples.js
+++ src/components/WorkExamples.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { projects } from '../projects';
+import Project from './Project';
 
 function WorkExamples(){
     const [isActive, setIsActive] = useState(false);
@@ -15,26 +15,7 @@ function WorkExamples(){
 
     return(
         <div id="portfolioExamples" className={isActive ? 'active' : ''}>
-            {Object.values(projects).map((project, index) => (
-                console.log(project.img),
-                <div key={index} className="project">
-                    <div className='project-screenshot'>
-                        <img src={project.img} alt={project.name} />
-                    </div>
-                    <div className='project-details'>
-                        <h3>{project.name}</h3>
-                        <div className='tech-badges'>
-                            {
-                                Array.isArray(project.tech) && project.tech.map((tech, index) => (
-                                    <img className='tech-badge' key={index} src={tech} alt={`Tech ${index + 1}`} />
-                                ))
-                            }
-                        </div>
-                        <p>{project.desc}</p>
-                        <a target="_blank" href={project.link}>Visit {project.name}</a>
-                    </div>
-                </div>
-            ))}
+            <Project />
         </div>
     )
 }
