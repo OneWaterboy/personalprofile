f6cbe7bd39d7eaff491e9795b3161353dd8af9fc
Waterboy <abrahmrollins@gmail.com>
2024-08-23 21:08:55 +1200

    Moving CSS rules to optimize styling - Too many issues with way it was done before - simplifying

--- src/components/ProfileIcons.js
+++ src/components/ProfileIcons.js
@@ -8,13 +8,11 @@ import H2HLogo from '../media/snowyridge.png';
 function ContactIcons(){
     return(
         <div className="contact-icons">
-            <div className="icons-box">
                 <a className="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
                 <a className="phone-icon" href="tel:640273923491" target="_blank" rel="noreferrer"><img src={PhoneIcon} alt="Cellphone Icon" /></a>
                 <a className="linkedin-icon" href="https://www.linkedin.com/in/abrahmrollins" target="_blank" rel="noreferrer"><img src={LinkedinIcon} alt="Github Icon" /></a>
                 <a className="hike2hike-icon" href="https://www.hike2hike.com" target="_blank" rel="noreferrer"><img src={H2HLogo} alt="Github Icon" /></a>
                 <a className="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
-            </div>
         </div>
     )
 }
