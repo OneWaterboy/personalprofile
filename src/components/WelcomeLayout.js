8ae72b5b4d1fd190495749596df607026f38e344
Waterboy <abrahmrollins@gmail.com>
2024-08-23 13:52:32 +1200

    Edited elevator pitch text

--- src/components/WelcomeLayout.js
+++ src/components/WelcomeLayout.js
@@ -22,13 +22,15 @@ function WelcomeLayout(){
                     <img src={ProfileImage} alt="Abrahm" id="abrahmImage" />
                 </div>
                 <h1>Hi, I’m Abrahm.</h1>
-                <p className="greeting-text"> 
-                    I’m a highly analytical and detail-oriented professional who loves mountaineering. 
-                    As I carefully plan and navigate challenging climbs, I apply the same meticulous approach to solving complex problems and analyzing data. 
-                    My mountaineering experiences have taught me resilience, strategic thinking, and the importance of precision—skills I bring to every project. 
-                    I thrive on turning data into actionable insights and delivering results with accuracy and clarity.
-                </p>
                 <ProfileIcons />
+                <p className="greeting-text"> 
+                    I’m a highly analytical and detail-oriented professional who loves spending my free time mountaineering. I've successfully summited over 100 challenging peaks, 
+                    which has honed my problem-solving skills and taught me the value of preparation and perseverance. 
+                    Just as I carefully plan and navigate challenging climbs, I apply the same meticulous approach to solving complex problems in my professional life. 
+                    My mountaineering experiences have taught me resilience, strategic thinking, and the importance of precision, and they have also taught me how to apply these skills 
+                    to every project and endeavor in my professional life.  
+                </p>
+                <p className="greeting-text">In short, I thrive on delivering results with unwavering accuracy and crystal-clear clarity.</p>
                 {/* <h1>Abrahm Rollins</h1>
                 <h2>Front End Developer &#183; Mountaineer</h2> */}
                 {/* <button id="showCarousel" className={isActive ? 'hide-button' : ''}>See My Work</button> */}
