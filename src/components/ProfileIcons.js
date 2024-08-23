efcb85340a72f52a0e3f51e3eaf23dfdd99d2005
Waterboy <abrahmrollins@gmail.com>
2024-08-23 12:42:42 +1200

    Changd Phone Number to New Zealand Number

--- src/components/ProfileIcons.js
+++ src/components/ProfileIcons.js
@@ -9,7 +9,7 @@ function ContactIcons(){
         <div className="contact-icons">
             <div className="icons-box">
                 <a className="email-icon" href="mailto:abrahmrollins@gmail.com" target="_blank" rel="noreferrer"><img src={EmailIcon} alt="Email Icon" /></a>
-                <a className="phone-icon" href="tel:12063956074" target="_blank" rel="noreferrer"><img src={PhoneIcon} alt="Cellphone Icon" /></a>
+                <a className="phone-icon" href="tel:640273923491" target="_blank" rel="noreferrer"><img src={PhoneIcon} alt="Cellphone Icon" /></a>
                 <a className="linkedin-icon" href="https://www.linkedin.com/in/abrahmrollins" target="_blank" rel="noreferrer"><img src={LinkedinIcon} alt="Github Icon" /></a>
                 <a className="github-icon" href="https://github.com/OneWaterboy" target="_blank" rel="noreferrer"><img src={GithubIcon} alt="Github Icon" /></a>
             </div>
