8018b2b508fee2792f28733fbb5a26961591abe9
Waterboy <abrahmrollins@gmail.com>
2024-08-26 10:58:19 +1200

    Changed info text to pop up block

--- src/components/InfoText.js
+++ src/components/InfoText.js
@@ -3,17 +3,17 @@ import { useState } from 'react';
 
 function InformationText({ isActive }) {
     return (
-        <div id="infoTextBox">
-            <p className={`greeting-text ${isActive ? 'active' : ''}`}>
+        <div id="infoTextBox" className={`${isActive ? 'active' : ''}`}>
+            <p className='greeting-text'>
                 I’m a highly analytical and detail-oriented professional who loves spending my free time mountaineering. I've successfully summited over 100 challenging peaks, 
                 which has honed my problem-solving skills and taught me the value of preparation and perseverance. 
                 Just as I carefully plan and navigate challenging climbs, I apply the same meticulous approach to solving complex problems in my professional life.
             </p>
-            <p className={`greeting-text ${isActive ? 'active' : ''}`}>
+            <p className='greeting-text'>
                 My mountaineering experiences have taught me resilience, strategic thinking, and the importance of precision, and they have also taught me how to apply these skills 
                 to every project and endeavor in my professional life.  
             </p>
-            <p className={`greeting-text ${isActive ? 'active' : ''}`}>
+            <p className='greeting-text'>
                 In short, I thrive on delivering results with unwavering accuracy and crystal-clear clarity.
             </p>
         </div>
